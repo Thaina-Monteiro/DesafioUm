528d2829c871bd9c8a3128b9ceca32f4df4ff9e4
Thainá Monteiro <96400582+Thaina-Monteiro@users.noreply.github.com>
2022-06-11 15:11:18 -0300

    DesafioUm

---
+++ desafioum.js
@@ -0,0 +1,8 @@
+let nome = 'Thainá';
+let numero = 23;
+let ano = 2022 - numero;
+console.log(nome, numero, ano);
+console.log("Estou mexendo no console.log");
+let quartaFeira = false;
+console.log(quartaFeira);
+console.log(typeof quartaFeira)
